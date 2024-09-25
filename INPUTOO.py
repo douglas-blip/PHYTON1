dfe6f3f33aa3042ec713f7e28c07b6fd40521b41
douglas-blip <douglas.jordao@sempreceub.com>
2024-09-25 10:13:04 -0300

    Add files via upload
    primeiro commit

---
+++ INPUTOO.py
@@ -0,0 +1,7 @@
+base = float(input("digite a base:"))
+altura = float(input("digite a altura:"))
+area = base*altura/2
+print ("base",base)
+print ("altura",altura)
+print("area:",area)
+print (f"area: {area:.3f}")
